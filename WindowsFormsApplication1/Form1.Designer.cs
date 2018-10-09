829774492cce54a14f103735d95956f6a4403387
ahmaddwi21 <ahmad.sulistiyo@students.amikom.ac.id>
2018-10-09 11:36:38 +0700

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.tbNIM = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbAlamat = new System.Windows.Forms.TextBox();
+            this.btnPerbaiki = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCariData = new System.Windows.Forms.Button();
+            this.tbPencarian = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(9, 386);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 0;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // tbNIM
+            // 
+            this.tbNIM.Location = new System.Drawing.Point(106, 12);
+            this.tbNIM.Name = "tbNIM";
+            this.tbNIM.Size = new System.Drawing.Size(140, 20);
+            this.tbNIM.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGrid);
+            this.groupBox1.Location = new System.Drawing.Point(9, 135);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(726, 188);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[Daftar Mahasiswa]";
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(3, 19);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(717, 163);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nama";
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(106, 50);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(309, 20);
+            this.tbNama.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Alamat";
+            // 
+            // tbAlamat
+            // 
+            this.tbAlamat.Location = new System.Drawing.Point(106, 86);
+            this.tbAlamat.Name = "tbAlamat";
+            this.tbAlamat.Size = new System.Drawing.Size(541, 20);
+            this.tbAlamat.TabIndex = 6;
+            // 
+            // btnPerbaiki
+            // 
+            this.btnPerbaiki.Location = new System.Drawing.Point(90, 386);
+            this.btnPerbaiki.Name = "btnPerbaiki";
+            this.btnPerbaiki.Size = new System.Drawing.Size(75, 23);
+            this.btnPerbaiki.TabIndex = 8;
+            this.btnPerbaiki.Text = "Perbaiki";
+            this.btnPerbaiki.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(171, 386);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 9;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(252, 10);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(75, 23);
+            this.btnCari.TabIndex = 10;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnCariData);
+            this.groupBox2.Controls.Add(this.tbPencarian);
+            this.groupBox2.Location = new System.Drawing.Point(9, 329);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(726, 51);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "[Pencarian Data]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Nama";
+            // 
+            // btnCariData
+            // 
+            this.btnCariData.Location = new System.Drawing.Point(645, 16);
+            this.btnCariData.Name = "btnCariData";
+            this.btnCariData.Size = new System.Drawing.Size(75, 23);
+            this.btnCariData.TabIndex = 12;
+            this.btnCariData.Text = "Cari";
+            this.btnCariData.UseVisualStyleBackColor = true;
+            // 
+            // tbPencarian
+            // 
+            this.tbPencarian.Location = new System.Drawing.Point(81, 19);
+            this.tbPencarian.Name = "tbPencarian";
+            this.tbPencarian.Size = new System.Drawing.Size(557, 20);
+            this.tbPencarian.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 421);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnPerbaiki);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbAlamat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbNIM);
+            this.Controls.Add(this.btnSimpan);
+            this.Name = "Form1";
+            this.Text = "Data Mahasiswa";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.TextBox tbNIM;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbAlamat;
+        private System.Windows.Forms.Button btnPerbaiki;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCariData;
+        private System.Windows.Forms.TextBox tbPencarian;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using Dapper;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection konn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:/Form Mahasiswa/Mahasiswa.mdb");
+        public Form1()
+        {
+            InitializeComponent();
+            load();
+
+        }
+
+        public void load()
+        {
+            OleDbCommand command = new OleDbCommand("select * from DataMhs", konn);
+            konn.Open();
+
+            OleDbDataReader reader = command.ExecuteReader();
+            DataTable mytable = new DataTable();
+
+            mytable.Load(reader);
+            konn.Close();
+            dataGrid.DataSource = mytable;
+        }
+
+        private void empty()
+        {
+            tbNIM.Text = string.Empty;
+            tbNama.Text = string.Empty;
+            tbAlamat.Text = string.Empty;
+
+        }
+
+        private void insertData()
+        {
+            try
+            {
+                konn.Execute("insert into DataMhs(NIM, nama, alamat) values('"+tbNIM.Text+"','"+tbNama.Text+"','"+tbAlamat.Text+"')");
+                empty();
+
+                load();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            insertData();
+        }
+    }
+}
